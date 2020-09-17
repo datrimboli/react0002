02da4975bdfbcb1f2a49b751821fc4b9e3a0f279
Dave Trimboli <davidatrimboli@gmail.com>
2020-09-17 19:27:38 -0400

    Updates

---
+++ precache-manifest.5ba6f713a50d3ddc5c2177d3a4903a9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c9e6e4bb6bcd0a55342f2bd49589105",
+    "url": "/react0002/index.html"
+  },
+  {
+    "revision": "b358cc760014231be819",
+    "url": "/react0002/static/css/main.b82fb247.chunk.css"
+  },
+  {
+    "revision": "ae04f83f9651c4d00d90",
+    "url": "/react0002/static/js/2.8b8776a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react0002/static/js/2.8b8776a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b358cc760014231be819",
+    "url": "/react0002/static/js/main.1ca42267.chunk.js"
+  },
+  {
+    "revision": "6e86ab31bc2cb98bbbe1",
+    "url": "/react0002/static/js/runtime-main.3b106a31.js"
+  }
+]);
